45b3565350065c09f219596e165f35f49d9e43fd
zzorn <zzorn@iki.fi>
2015-07-02 13:09:56 +0300

    Refactored ColorGradient constructors and ColorGradients.

--- src/main/java/org/uiflow/desktop/gradient/ColorGradient.java
+++ src/main/java/org/uiflow/desktop/gradient/ColorGradient.java
@@ -1,12 +1,14 @@
 package org.uiflow.desktop.gradient;
 
 
-import org.uiflow.desktop.ColorUtils;
+import org.flowutils.MathUtils;
 
 import java.awt.*;
+import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
+import static org.flowutils.Check.notNull;
 import static org.flowutils.MathUtils.map;
 import static org.flowutils.MathUtils.mix;
 import static org.uiflow.desktop.ColorUtils.mixColors;
@@ -17,6 +19,40 @@ import static org.uiflow.desktop.ColorUtils.mixColors;
 public class ColorGradient extends ColorFunctionBase {
 
     private final TreeMap<Double, Color> colors = new TreeMap<Double, Color>();
+
+    private boolean allowModification = true;
+
+    /**
+     * @param colors initial colors in the gradient, spaced from 0 to 1.
+     */
+    public ColorGradient(Color ... colors) {
+        if (colors != null && colors.length > 0) {
+            addColors(colors);
+        }
+    }
+
+    /**
+     * Makes a copy of another ColorGradient.
+     */
+    public ColorGradient(ColorGradient source) {
+        notNull(source, "source");
+        final Map<Double, Color> sourceColors = source.getColors();
+        for (Map.Entry<Double, Color> sourceEntry : sourceColors.entrySet()) {
+            addColor(sourceEntry.getKey(), sourceEntry.getValue());
+        }
+    }
+
+    /**
+     * @param colors colors to add, spaced at equal distances from 0 to 1.
+     */
+    public void addColors(Color[] colors) {
+        checkModificationAllowed();
+        for (int i = 0; i < colors.length; i++) {
+            double pos = MathUtils.relPos(i, 0, colors.length - 1);
+            Color color = colors[i];
+            addColor(pos, color);
+        }
+    }
 
     /**
      * Adds a color to the gradient
@@ -53,6 +89,9 @@ public class ColorGradient extends ColorFunctionBase {
      * @param color color for the specified value
      */
     public void addColor(double value, Color color) {
+        checkModificationAllowed();
+        notNull(color, "color");
+
         colors.put(value, color);
     }
 
@@ -60,6 +99,8 @@ public class ColorGradient extends ColorFunctionBase {
      * Removes the color closest to the specified value.
      */
     public void removeColor(double value) {
+        checkModificationAllowed();
+
         final Map.Entry<Double, Color> floor = colors.floorEntry(value);
         final Map.Entry<Double, Color> ceiling = colors.ceilingEntry(value);
 
@@ -86,6 +127,7 @@ public class ColorGradient extends ColorFunctionBase {
      * Removes all colors from the gradient.
      */
     public void clear() {
+        checkModificationAllowed();
         colors.clear();
     }
 
@@ -93,8 +135,26 @@ public class ColorGradient extends ColorFunctionBase {
      * @return the underlying treemap.
      * Modifications to it are allowed, although not normally necessary.
      */
-    public TreeMap<Double, Color> getColors() {
+    public TreeMap<Double, Color> getColorsForEditing() {
+        checkModificationAllowed();
         return colors;
+    }
+
+    /**
+     * @return the underlying treemap.
+     * Modifications to it are allowed, although not normally necessary.
+     */
+    public Map<Double, Color> getColors() {
+        return Collections.unmodifiableMap(colors);
+    }
+
+    /**
+     * Makes this ColorGradient immutable.  Any modification operations will throw an exception after this.
+     * @return this gradient.
+     */
+    public ColorGradient makeReadOnly() {
+        allowModification = false;
+        return this;
     }
 
     /**
@@ -133,5 +193,9 @@ public class ColorGradient extends ColorFunctionBase {
 
     private int mixComponent(double colorAmount, double t, final int base, final int floorComp, final int ceilingComp) {
         return (int) mix(colorAmount, base, mix(t, floorComp, ceilingComp));
+    }
+
+    private void checkModificationAllowed() {
+        if (!allowModification) throw new UnsupportedOperationException("Modification of this ColorGradient is not permitted");
     }
 }
--- src/main/java/org/uiflow/desktop/gradient/ColorGradients.java
+++ src/main/java/org/uiflow/desktop/gradient/ColorGradients.java
@@ -6,12 +6,46 @@ import java.awt.*;
 /**
  * Utility class with some common color gradients.
  */
-public class ColorGradients {
+public final class ColorGradients {
 
     public static final ColorGradient SPECTRUM = createSpectrumGradient();
-    public static final ColorGradient RAINBOW = createRainbowGradient();
-    public static final ColorGradient BLUERED = createBlueRedGradient();
-    public static final ColorGradient GREYSCALE = createGreyscaleGradient();
+
+    public static final ColorGradient RAINBOW = new ColorGradient(
+            new Color(200, 0, 130),
+            new Color(255, 0, 0),
+            new Color(255, 128, 0),
+            new Color(255, 220, 0),
+            new Color(150, 220, 0),
+            new Color(0, 200, 0),
+            new Color(0, 180, 120),
+            new Color(0, 150, 255),
+            new Color(0, 60, 255),
+            new Color(0, 0, 255),
+            new Color(150, 0, 230)
+    ).makeReadOnly();
+
+    public static final ColorGradient BLUERED = new ColorGradient(
+            new Color(0, 0, 255),
+            new Color(80, 160, 255),
+            new Color(220, 220, 220),
+            new Color(255, 160, 80),
+            new Color(255, 0, 0)
+    ).makeReadOnly();
+
+    public static final ColorGradient GREYSCALE = new ColorGradient(
+            new Color(0, 0, 0),
+            new Color(255, 255, 255)
+    ).makeReadOnly();
+
+    public static final ColorGradient WARM_TO_COLD = new ColorGradient(
+            new Color(255, 220, 0),
+            new Color(255, 140, 0),
+            new Color(230, 50, 0),
+            new Color(200, 0, 130),
+            new Color(150, 0, 160),
+            new Color(100, 0, 200),
+            new Color(50, 0, 230)
+    ).makeReadOnly();
 
     public static ColorGradient createSpectrumGradient() {
         final ColorGradient gradient = new ColorGradient();
@@ -26,39 +60,6 @@ public class ColorGradients {
         gradient.addColor(0.8, 0, 0.5, 1);
         gradient.addColor(0.9, 0, 0, 1);
         gradient.addColor(1.0, 0.5, 0, 1);
-        return gradient;
-    }
-
-    public static ColorGradient createRainbowGradient() {
-        final ColorGradient gradient = new ColorGradient();
-        gradient.addColor(0.0, new Color(200,0,130));
-        gradient.addColor(0.1, new Color(255,0,0));
-        gradient.addColor(0.2, new Color(255,128,0));
-        gradient.addColor(0.3, new Color(255, 220, 0));
-        gradient.addColor(0.4, new Color(150, 220, 0));
-        gradient.addColor(0.5, new Color(0, 200, 0));
-        gradient.addColor(0.6, new Color(0, 180, 120));
-        gradient.addColor(0.7, new Color(0, 150, 255));
-        gradient.addColor(0.8, new Color(0, 60, 255));
-        gradient.addColor(0.9, new Color(0, 0, 255));
-        gradient.addColor(1.0, new Color(150, 0, 230));
-        return gradient;
-    }
-
-    public static ColorGradient createGreyscaleGradient() {
-        final ColorGradient gradient = new ColorGradient();
-        gradient.addColor(0.0, new Color(0,0,0));
-        gradient.addColor(1.0, new Color(255, 255, 255));
-        return gradient;
-    }
-
-    public static ColorGradient createBlueRedGradient() {
-        final ColorGradient gradient = new ColorGradient();
-        gradient.addColor(0.0,  new Color(0, 0, 255));
-        gradient.addColor(0.25, new Color(80, 160, 255));
-        gradient.addColor(0.5,  new Color(220, 220, 220));
-        gradient.addColor(0.75, new Color(255, 160, 80));
-        gradient.addColor(1.0,  new Color(255, 0, 0));
         return gradient;
     }
 
